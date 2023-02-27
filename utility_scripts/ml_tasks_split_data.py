1a769d67c65fe465dccad30f79dfdb8baeb019b6
Tarek Saier <tareksaier@gmail.com>
2023-02-27 17:46:18 +0100

    implement smpls per label aware ML data splitting
    - make sure that for every label with samples in
      test data, there is a least one sample in val
      and train data
    - remove sample debug data before persisting
    - persist splits as JSONL files

--- utility_scripts/ml_tasks_split_data.py
+++ utility_scripts/ml_tasks_split_data.py
@@ -1,5 +1,6 @@
 import json
 import math
+import os
 import random
 import sys
 from collections import defaultdict
@@ -11,14 +12,12 @@ def split(fn):
 
     dev_size_min_smpls = 1000
     test_size_min_smpls = 1000
+    splits = ['test', 'dev', 'train']  # in fill order
 
     # determine sample and label key
     for key in ['imrad_smpls', 'citrec_smpls']:
         if key in smpl_packs[0]:
             sample_key = key
-    for key in ['label', 'cited_doc']:
-        if key in smpl_packs[0][sample_key][0]:
-            label_key = key
 
     # calculate sample allocation goals
     dists_abs = {}
@@ -26,19 +25,46 @@ def split(fn):
     test_fill_min = {}
     dev_fill_curr = {}
     test_fill_curr = {}
-    strat_dimensions = ['year', 'discipline', label_key]
+    strat_dimensions = ['year', 'discipline', 'label']
+    num_packets_for_label = defaultdict(int)
     for strat in strat_dimensions:
         dists_abs[strat] = defaultdict(int)
         dev_fill_min[strat] = defaultdict(int)
         test_fill_min[strat] = defaultdict(int)
         dev_fill_curr[strat] = defaultdict(int)
         test_fill_curr[strat] = defaultdict(int)
+    # # determine usable labels
+    for ppr in smpl_packs:
+        uniq_lbls = set()
+        for smpl in ppr[sample_key]:
+            uniq_lbls.add(smpl['label'])
+        for lbl in uniq_lbls:
+            num_packets_for_label[lbl] += 1
+    usable_labels = [
+        k for (k, v) in num_packets_for_label.items()
+        if v >= len(splits)
+    ]
+    # # filter samples to only contain usable labels
+    smpl_packs_usable = []
+    for ppr in smpl_packs:
+        smpls_usable = []
+        for smpl in ppr[sample_key]:
+            if smpl['label'] in usable_labels:
+                smpls_usable.append(smpl)
+        if len(smpls_usable) > 0:
+            ppr_usable = {}
+            for k, v in ppr.items():
+                if k != sample_key:
+                    ppr_usable[k] = v
+            ppr_usable[sample_key] = smpls_usable
+            smpl_packs_usable.append(ppr_usable)
+    smpl_packs = smpl_packs_usable
     # # determine total distribution of stratification dimensions
     for ppr in smpl_packs:
         dists_abs['year'][ppr['year']] += len(ppr[sample_key])
         dists_abs['discipline'][ppr['discipline']] += len(ppr[sample_key])
         for smpl in ppr[sample_key]:
-            dists_abs[label_key][smpl[label_key]] += 1
+            dists_abs['label'][smpl['label']] += 1
     # # determine relative distribution of stratification dimensions
     # # and calculate allocation minima
     smpls_total = sum(n for n in dists_abs['year'].values())
@@ -52,7 +78,6 @@ def split(fn):
                 test_size_min_smpls * rel_size
             )
     # allocate samples
-    splits = ['test', 'dev', 'train']  # in fill order
     smpls_split = {}
     for split in splits:
         smpls_split[split] = []
@@ -68,7 +93,16 @@ def split(fn):
     random.shuffle(smpl_packs)
     for ppr in smpl_packs:
         added = False
-        for split in splits[:-1]:  # put in test or dev
+        for i, split in enumerate(splits[:-1]):  # put in test or dev
+            # check if labels to be allocated have enough remaining samples
+            num_other_splits = len(splits) - i - 1
+            cant_use = False
+            for smpl in ppr[sample_key]:
+                if num_packets_for_label[smpl['label']] <= num_other_splits:
+                    cant_use = True
+                    break
+            if cant_use:
+                continue
             # check if current paper samples are useful to add to split
             num_strat_dims_needed = 0
             for strat in strat_dimensions:  # for all dims
@@ -83,7 +117,9 @@ def split(fn):
                     num_strat_dims_needed += 1
             # add if useful
             if num_strat_dims_needed == len(strat_dimensions):
-                smpls_split[split].extend(ppr[sample_key])
+                smpls_split[split].extend(
+                    remove_debug_fields(ppr[sample_key])
+                )
                 added = True
                 # keep track of allocation numbers
                 split_currs[split]['year'][ppr['year']] += len(
@@ -93,21 +129,26 @@ def split(fn):
                     ppr[sample_key]
                 )
                 for smpl in ppr[sample_key]:
-                    split_currs[split][label_key][smpl[label_key]] += 1
+                    split_currs[split]['label'][smpl['label']] += 1
+                # keep track of packets left per label
+                for lbl in set([s['label'] for s in ppr[sample_key]]):
+                    num_packets_for_label[lbl] -= 1
                 # don’t add to other splits
                 break
         # add to train if “not needed” in tran/dev
         if not added:
-            smpls_split['train'].extend(ppr[sample_key])
+            smpls_split['train'].extend(
+                remove_debug_fields(ppr[sample_key])
+            )
 
     # for split in splits[:-1]:
     #     print(split)
-    #     split_smpls_total = len(smpls_split[split])
-    #     print(f'\tsamples total: {split_smpls_total}')
+    #     split_total = len(smpls_split[split])
+    #     print(f'\tsamples total: {split_total}')
     #     for strat in strat_dimensions:
     #         for k in dists_abs[strat].keys():
     #             rel_of_total = dists_abs[strat][k] / smpls_total
-    #             rel_of_split = split_currs[split][strat][k] / split_smpls_total
+    #             rel_of_split = split_currs[split][strat][k] / split_total
     #             print((
     #                 f'\t\t{strat}-{k}: {rel_of_total:.4f} '
     #                 f'-> {rel_of_split:.4f}'
@@ -115,8 +156,28 @@ def split(fn):
     #             ))
     #             input()
 
-    # TODO
-    # - persist
+    fn_base, ext = os.path.splitext(os.path.split(fn)[-1])
+    for split, smpls in smpls_split.items():
+        fn = f'{fn_base}_{split}.jsonl'
+        with open(fn, 'w') as f:
+            for smpl in smpls:
+                f.write(json.dumps(smpl) + '\n')
+
+
+def remove_debug_fields(smpls):
+    """ Remove dict fields starting with _
+    """
+
+    clean_smpls = []
+    for smpl in smpls:
+        clean_smpls.append(
+            {
+                k: v for (k, v)
+                in smpl.items()
+                if k[0] != '_'
+            }
+        )
+    return clean_smpls
 
 
 if __name__ == '__main__':
