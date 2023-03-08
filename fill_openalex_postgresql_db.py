beafd99cbb2abc2d3da0ba2e7c36f4b5499f7563
johankit <krausejohan96@gmail.com>
2023-03-08 19:28:23 +0100

    Revised and extended OpenAlex table 'papers' creation. Includes arxiv_id matching in oa_url and discipline extraction

--- fill_openalex_postgresql_db.py
+++ fill_openalex_postgresql_db.py
@@ -28,104 +28,107 @@ def normalize_author_name(author_string):
     author_string_norm = unidecode.unidecode(author_string_norm)
     return author_string_norm.strip().lower()
 
+
+PATT_PS_CACHE_NEW_ID = re.compile('arxiv.org\/PS_cache\/arxiv\/pdf\/(.+?)(v\d+)?\.pdf', re.I)
+PATT_PS_CACHE_OLD_ID = re.compile('arxiv.org\/PS_cache\/([a-z-]+)\/pdf\/(.+?)(v\d+)?\.pdf', re.I)
+PATT_EXT = re.compile('arxiv.org\/[a-z]+\/(.+)((.pdf)|(\?.*$)|(v\d+))', re.I)
+PATT_NORM = re.compile('arxiv.org\/[a-z]+\/(.+)$', re.I)
+PATT_DOI = re.compile(r'10.\d{4,9}/[-._;()/:A-Z0-9]+$', re.I)
+
 # check size of db
 # psql openalex
-# SELECT pg_size_pretty (pg_database_size ('openalex'));
+# SELECT pg_size_pretty (pg_database_size ('papers'));
+# Length:
+# SELECT COUNT(1) FROM papers;
 
+# find entries with arxive ID
+# SELECT COUNT(*) FROM papers WHERE LENGTH(arxiv_id) >0;
 
-#test the connection
+# TMUX
+# tmux ls
+# tmux at -t mysession
+# detach: Crtl + B, then D
+
+# method for extracting arxive IDs from url
+def extract_arxiv_id_from_url(url):
+    arxiv_id = ""
+    success_flag = False
+    aid_group = PATT_EXT.search(url)
+    if not aid_group:
+        aid_group = PATT_NORM.search(url)
+    if not aid_group:
+        aid_group = PATT_PS_CACHE_NEW_ID.search(url)
+    if aid_group:
+        arxiv_id = aid_group.group(1)
+    else:
+        aid_group2 = PATT_PS_CACHE_OLD_ID.search(url)
+        if not aid_group2:
+            print("Problem in: ", url)
+        arxiv_id = '{}{}'.format(aid_group2.group(1), aid_group2.group(2))
+    if len(arxiv_id) != 0:
+        success_flag = True
+    return success_flag, arxiv_id
+
+# test the connection
 conn = psycopg2.connect(database="openalex")
 cursor = conn.cursor()
 cursor.execute("select version()")
 
-#Fetch a single row using fetchone() method.
+# Fetch a single row using fetchone() method.
 data = cursor.fetchone()
 print("Connection established to: ", data)
 
-
-# remove previous instance of openalex table if existing
+# remove previous instance of table "papers" if existing
 
 cursor = conn.cursor()
-cursor.execute("DROP TABLE IF EXISTS openalex")
+cursor.execute("DROP TABLE IF EXISTS papers")
 conn.commit()
-
-#cursor.execute("DROP TABLE IF EXISTS openalex")
 
 conn = psycopg2.connect(database="openalex")
 cursor = conn.cursor()
 cursor.execute('''
-        CREATE TABLE openalex (
+        CREATE TABLE papers (
             number SERIAL PRIMARY KEY,
+            openalex_id VARCHAR,
             normalized_title VARCHAR,
             authors VARCHAR[],
+            discipline_name VARCHAR,
+            discipline_url VARCHAR,
             citation_count INTEGER,
-            ids VARCHAR[]
+            ids VARCHAR[],
+            doi VARCHAR,
+            oa_url VARCHAR,
+            arxiv_id VARCHAR
         )
 ''')
 conn.commit()
 
-print("Table created successfully.....")
-
-# fill testwise
-'''
-json_raw = {'open_alex_id': 987, 'pmc_id': 123, 'pubmed_id': 555, 'doi': 'http://doiurl.com'}
-
-s = ''
-for v in json_raw.values():
-    s += '"' + str(v) + '",'
-s = "{" + str(s)[:-1] + "}"
-
-
-a = '{"author1", "author2"}'
-conn = psycopg2.connect(database="openalex")
-cursor = conn.cursor()
-print("inserting...")
-cursor.execute("INSERT INTO openalex (number, normalized_title, authors, citation_count, ids) VALUES (%s,%s,%s,%s,%s)",
-               (4, 'Titel', a, 75, s))
-
-conn.commit()
-conn.close()
-
-conn = psycopg2.connect(database="openalex")
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM openalex")
-result = cursor.fetchone()
-print(result)
-conn.commit()
-conn.close()
-'''
+print("Table 'papers' created successfully.")
 
 i = 0
 error_count = 0
 file_count = 0
-# recall openalexdb IDs column structure: list of IDs [oa_id, pubmedid, pmcid, doi]
-
-# in db:
-# (1, 'otello u shekspira na armianskoi stsene ai bem v monreale', ['anait bekarian'], 0, ['https://openalex.org/W4297237744', 'https://semopenalex.org/work/W4297237744', '', '', 'https://doi.org/10.54503/1829-4073-2022.2.118-129'])
-# (3, 'premature primary tooth loss and oral health related quality of life in preschool children', ['monalisa cesarino gomes', 'matheus franca perazzo', 'ana flavia granville garcia'], 0, ['W4297237785', 'https://pubmed.ncbi.nlm.nih.gov/36231465', '', 'https://doi.org/10.3390/ijerph191912163'])
+arxiv_id_matching_success_count = 0
 
 input_dir_openalex_works_files = r'/opt/unarXive_2022/openalex/openalex-works-2022-11-28/*'
 print(f"Working in directory: {input_dir_openalex_works_files}")
 
 for filename in glob.glob(os.path.join(input_dir_openalex_works_files, '*.gz')):
-    print("Processing file..",filename)
+    print("Processing file..", filename)
     file_count += 1
-    print(f"File {file_count} of",len(glob.glob(os.path.join(input_dir_openalex_works_files, '*.gz'))))
+    print(f"File {file_count} of", len(glob.glob(os.path.join(input_dir_openalex_works_files, '*.gz'))))
 
     with gzip.open(filename, 'r') as f:
         for line in f:
+
             try:
                 i += 1
                 json_data = json.loads(line.decode('utf-8'))
-
-                # get data from current json line for current object
 
                 work_title_orig = json_data['title']
 
                 # normalize title
                 if work_title_orig is not None:
-                    # example orig title: Reversible Mechanochromic Luminescence of [(C6F5Au)2(μ-1,4-Diisocyanobenzene)]
-                    # normalized title: reversible mechanochromic luminescence of c6f5au 2 m 1 4 diisocyanobenzene
                     work_title_norm = normalize_title(work_title_orig)
 
                 else:
@@ -134,21 +137,17 @@ for filename in glob.glob(os.path.join(input_dir_openalex_works_files, '*.gz')):
                 work_authorships = json_data['authorships']
                 work_author_list = []
                 for author in work_authorships:
-                    # normalize author name for every involved author
-                    work_author_norm = normalize_author_name(author['author']['display_name']).replace("'","").replace('"','')
+                    work_author_norm = normalize_author_name(author['author']['display_name']).replace("'",
+                                                                                                       "").replace(
+                        '"', '')
                     work_author_list.append(work_author_norm)
 
-                # modify string so that it suits postgresql syntax to insert into table as a list of strings
                 work_author_list_str = str(work_author_list).replace('[', '{').replace(']', '}').replace("'", '"')
-                # example list str now:
-                # {"engelbert knosp", "erich steiner", "klaus kitz", "christian matula"}
 
                 work_cited_by_count = json_data['cited_by_count']
 
                 ids = json_data.get('ids')
-                # example ids list: {'openalex': 'https://openalex.org/W2169161843', 'doi': 'https://doi.org/10.1177/1362361312472989', 'pmid': 'https://pubmed.ncbi.nlm.nih.gov/23614935', 'mag': 2169161843}
                 ids_list = []
-                # wanted: openalex, semopenalex, pubmed, pmc, doi
 
                 if not ids['openalex'] is None:
                     ids_list.append(ids['openalex'].replace("https://openalex.org/", ""))
@@ -174,38 +173,94 @@ for filename in glob.glob(os.path.join(input_dir_openalex_works_files, '*.gz')):
                 else:
                     ids_list.append("")
 
-                # adapt to postgresql syntax to insert into table as a list of strings
                 ids_list = str(ids_list).replace('[', '{').replace(']', '}').replace("'", '"')
 
-                # write data of current object to database table
-                # write the values number, work_title_norm, work_author_list_str, work_cited_by_count, ids_list
+                #### NEW! ###
+                openalex_id = ids['openalex'].replace("https://openalex.org/", "")
+
+                # DOI value (not URL)
+                if not doi is None:
+                    doi_short = PATT_DOI.search(doi)
+                    if doi_short is not None:
+                        if len(doi_short[0]) != 0:
+                            doi_short = doi_short[0]
+                            #print(doi_short[0])
+                    else:
+                        doi_short = ""
+
+                # Most weighted level-0 Concept: display name and wikidata URL
+
+                concept_list = json_data['concepts']
+                # print(concept_list)
+                if concept_list is not None:
+                    concept_name = None
+                    for concept_item in concept_list:
+                        if concept_name is None:
+                            if concept_item['level'] == 0:
+                                concept_name = concept_item['display_name']
+                                concept_url = concept_item['wikidata']
+                                # print(concept_name, concept_url)
+                                # hardcore re-naming to capitalize concepts
+                                if concept_name == "Political science":
+                                    concept_name = "Political Science"
+                                if concept_name == "Computer science":
+                                    concept_name = "Computer Science"
+                                if concept_name == "Materials science":
+                                    concept_name = "Materials Science"
+                                if concept_name == "Environmental science":
+                                    concept_name = "Environmental Science"
+
+                    if concept_name is None:
+                        concept_name = ""
+                        concept_url = ""
+
+
+                ### open access link + potentially arxiv ID from it
+                oa_info = json_data['open_access']
+                arxiv_id = ""
+                oa_url = ""
+
+                if oa_info is not None:
+                    if oa_info['oa_url'] is not None:
+                        oa_url = oa_info['oa_url']
+                        if "arxiv" in oa_url:  # optional check?!
+
+                            aid_group = PATT_EXT.search(oa_url)
+                            success, arxiv_id = extract_arxiv_id_from_url(oa_url)
+                            if success:
+                                arxiv_id_matching_success_count += 1
+
 
                 conn = psycopg2.connect(database="openalex")
                 cursor = conn.cursor()
                 cursor.execute(
-                    "INSERT INTO openalex (number, normalized_title, authors, citation_count, ids) VALUES (%s,%s,%s,%s,%s)",
-                    (i, work_title_norm, work_author_list_str, work_cited_by_count, ids_list))
+                    "INSERT INTO papers (number, openalex_id, normalized_title, authors, discipline_name, discipline_url, citation_count, ids, doi, oa_url, arxiv_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                    (i, openalex_id, work_title_norm, work_author_list_str, concept_name, concept_url, work_cited_by_count, ids_list, doi_short, oa_url, arxiv_id))
 
-                if i % 50000 == 0:
-                    print(f"Inserting line {i} into table..")
+                """
+                # example insert:
+                # (15000, 'W4232445700', 'introduction', '{"jeffrey a brown"}', 'Philosophy', 'https://www.wikidata.org/wiki/Q5891', 0, 
+                # '{"W4232445700", "", "", "https://doi.org/10.14325/mississippi/9781604737141.003.0001"}', '10.14325/mississippi/9781604737141.003.0001', '', '')
+                """
 
                 conn.commit()
                 conn.close()
 
             except Exception as e:
-                #print(f'{e} error in line {i} of file {filename}')
-                #print('faulty data in:')
-                #print(json_data['title'],json_data['authorships'] )
+                print(f'{e} error in line {i} of file {filename}')
+                # print('faulty data in:')
+                # print(json_data['title'],json_data['authorships'] )
                 # note: errors in most cases due to missing author name in OpenAlex data
 
                 error_count += 1
                 pass
 
-
-
+            if i % 1000000 == 0:
+                print(f"Processed line #{i}")
 
 print("------ DONE ------")
-print(f"Processed {i} lines" )
+print(f"Processed {i} lines")
 print(f"Encountered {error_count} errors")
-print("Success rate for reading and writing to postgresql db therefore: {:.2f}".format(100*((i-error_count)/i)))
+print("Success rate for reading and writing to postgresql db therefore: {:.2f}".format(100 * ((i - error_count) / i)))
+print(f"arxiv IDs determined through oa_url: {arxiv_id_matching_success_count} -- a rate of",(arxiv_id_matching_success_count/i))
 print("#########")
