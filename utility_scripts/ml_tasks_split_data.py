90ddd126acb1d427d5acb60f47adec46ec46de81
ys8950 <ys8950@aifb-ls3-icarus.aifb.kit.edu>
2023-03-25 10:10:20 +0100

    pre-alloc 1 sample per class in ML data splitting
    stratified train/dev/test splitting with a huge
    number of classes (i.e. our citrec data where
    each cited paper ID is a class) resulted in
    classes not being represented in all sets. now
    one sample per class is pre-allocated to each
    split.

--- utility_scripts/ml_tasks_split_data.py
+++ utility_scripts/ml_tasks_split_data.py
@@ -31,8 +31,10 @@ def split(fn_to_split, fn_license_info, dev_test_size, single_disc):
         grp_stat
     """
 
+    print('reading paper data')
     with open(fn_to_split) as f:
         smpl_packs = json.load(f)
+    print('reading license data')
     with open(fn_license_info) as f:
         paper_license_dict = json.load(f)
     license_dict_dist = {}
@@ -46,6 +48,9 @@ def split(fn_to_split, fn_license_info, dev_test_size, single_disc):
     dev_size_min_smpls = dev_test_size
     test_size_min_smpls = dev_test_size
     splits = ['test', 'dev', 'train']  # in fill order
+    smpls_split = {}
+    for split in splits:
+        smpls_split[split] = []
 
     # determine sample and label key
     for key in ['imrad_smpls', 'citrec_smpls']:
@@ -53,6 +58,7 @@ def split(fn_to_split, fn_license_info, dev_test_size, single_disc):
             sample_key = key
 
     # calculate sample allocation goals
+    print('calculating sample allocation goals')
     dists_abs = {}
     dev_fill_min = {}
     test_fill_min = {}
@@ -66,31 +72,81 @@ def split(fn_to_split, fn_license_info, dev_test_size, single_disc):
         test_fill_min[strat] = defaultdict(int)
         dev_fill_curr[strat] = defaultdict(int)
         test_fill_curr[strat] = defaultdict(int)
+    train_fill_curr_label = defaultdict(int)  # only used in pre-fill
     # # determine usable labels
+    print('determining usable labels')
     for ppr in smpl_packs:
+        # determine set of unique labels for which we have
+        # samples for the current paper
         uniq_lbls = set()
         for smpl in ppr[sample_key]:
             uniq_lbls.add(smpl['label'])
+        # for each unique label, increase count of papers
+        # for label by 1
         for lbl in uniq_lbls:
             num_packets_for_label[lbl] += 1
+    # usable labels are those for which we have samples
+    # from at least <num_splits> (i.e. 3) papers
     usable_labels = [
         k for (k, v) in num_packets_for_label.items()
         if v >= len(splits)
     ]
     # # filter samples to only contain usable labels
+    print('filtering out unusable labels')
     smpl_packs_usable = []
+    # # shuffle b/c we’ll already pre-assign some sample to splits
+    random.seed(42)
+    random.shuffle(smpl_packs)
     for ppr in smpl_packs:
+        # prepare usable subset of paper samples
         smpls_usable = []
         for smpl in ppr[sample_key]:
-            if smpl['label'] in usable_labels:
+            lbl = smpl['label']
+            if lbl in usable_labels:
+                # pre-allocation to ensure each usable sample appears
+                # at least once in each split
+                for (split_key, fill_counter) in [
+                    ('test', test_fill_curr['label']),
+                    ('dev', dev_fill_curr['label']),
+                    ('train', train_fill_curr_label),
+                ]:
+                    if fill_counter[lbl] == 0:
+                        clean_smpls = clean_samples(
+                            [smpl],  # function expects a list of samples
+                            paper_license_dict,
+                            license_dict_dist
+                        )
+                        smpls_split[split_key].extend(clean_smpls)
+                        fill_counter[lbl] += 1
+                        break  # don’t assign to other splits
+                # rest is used for stratified samples
                 smpls_usable.append(smpl)
+        # if paper contains any usable samples
         if len(smpls_usable) > 0:
+            # re-create paper with only usable samples
             ppr_usable = {}
             for k, v in ppr.items():
                 if k != sample_key:
                     ppr_usable[k] = v
             ppr_usable[sample_key] = smpls_usable
             smpl_packs_usable.append(ppr_usable)
+    # print('num usable labels:')
+    # print(len(usable_labels))
+    # print('num smpl packs:')
+    # print(len(smpl_packs))
+    # print('num usable smpl packs:')
+    # print(len(smpl_packs_usable))
+    # import pprint
+    # for (split_key, fill_counter) in [
+    #     ('test', test_fill_curr['label']),
+    #     ('dev', dev_fill_curr['label']),
+    #     ('train', train_fill_curr_label),
+    # ]:
+    #     print(f'{split_key}:')
+    #     pprint.pprint(fill_counter)
+    # for split_name, smpls in smpls_split.items():
+    #     print(f'#{split_name} samples: {len(smpls)}')
+    # sys.exit()
     smpl_packs = smpl_packs_usable
     # # determine total distribution of stratification dimensions
     for ppr in smpl_packs:
@@ -111,9 +167,6 @@ def split(fn_to_split, fn_license_info, dev_test_size, single_disc):
                 test_size_min_smpls * rel_size
             )
     # allocate samples
-    smpls_split = {}
-    for split in splits:
-        smpls_split[split] = []
     split_mins = {
         'test': test_fill_min,
         'dev': dev_fill_min,
@@ -122,8 +175,6 @@ def split(fn_to_split, fn_license_info, dev_test_size, single_disc):
         'test': test_fill_curr,
         'dev': dev_fill_curr,
     }
-    random.seed(42)
-    random.shuffle(smpl_packs)
     for ppr in smpl_packs:
         added = False
         for i, split in enumerate(splits[:-1]):  # put in test or dev
@@ -194,14 +245,14 @@ def split(fn_to_split, fn_license_info, dev_test_size, single_disc):
     #                 f' ({split_currs[split][strat][k]})'
     #             ))
 
-    # persist samples
+    # save samples
     fn_base, ext = os.path.splitext(os.path.split(fn_to_split)[-1])
     for split, smpls in smpls_split.items():
         fn = f'{fn_base}_{split}.jsonl'
         with open(fn, 'w') as f:
             for smpl in smpls:
                 f.write(json.dumps(smpl) + '\n')
-    # persist license info
+    # save license info
     fn_license = f'{fn_base}_license_info.jsonl'
     with open(fn_license, 'w') as f:
         for arxiv_id, license_info in license_dict_dist.items():
